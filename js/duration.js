92adeaec8648792945fa4bde00039c0cb068ec38
lianghuan <lianghuanA@outlook.com>
2024-12-06 10:05:40 +0800

    同步更新

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function () {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2024/02/07 00:00:00");
+  var start = new Date("2020/09/11 00:00:00");
 
   function update() {
     var now = new Date();
